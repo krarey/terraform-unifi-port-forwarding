c13dca470dcb4d9290f4d3de73452b5d07f9b753
Kyle Rarey <kyle.rarey@gmail.com>
2022-08-31 23:50:34 -0400

    Pull source port from meta

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "unifi_port_forward" "service" {
   for_each               = var.services
+  enabled                = true
   name                   = each.value.id
   src_ip                 = "any"
   fwd_ip                 = each.value.address
-  fwd_port               = each.value.port
+  fwd_port               = each.value.meta.unifi-port-forward
   dst_port               = each.value.port
   protocol               = each.value.meta.unifi-proto
   port_forward_interface = "wan"
