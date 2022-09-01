0c6b74bd775518a5968c0575e81d3cf2657d2b93
Kyle Rarey <kyle.rarey@gmail.com>
2022-08-31 23:56:36 -0400

    Flipped forward and destination ports

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "unifi_port_forward" "service" {
   name                   = each.value.id
   src_ip                 = "any"
   fwd_ip                 = each.value.address
-  fwd_port               = each.value.meta.unifi-port-forward
-  dst_port               = each.value.port
+  fwd_port               = each.value.port
+  dst_port               = each.value.meta.unifi-port-forward
   protocol               = each.value.meta.unifi-proto
   port_forward_interface = "wan"
 }
